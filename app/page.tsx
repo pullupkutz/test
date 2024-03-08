19768bff241decce92c51db777211bfa3c12c2a2
Pull Up Kutz <pullupkutz@shons-air.myfiosgateway.com>
2024-03-08 07:33:28 -0500

    updated app

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex justify-center bg-green-300 w-full text-3xl">
       <h2>Let's build a test app</h2>
+      <h3>what to do</h3>
     </main>
   );
 }
