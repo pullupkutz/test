ddb3ebdf18d1fa7629903f640d71f42a6b262375
Pull Up Kutz <pullupkutz@shons-air.myfiosgateway.com>
2024-03-12 03:53:40 -0400

    updated app

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 
+import Navbar from "./Components/Navbar";
+
 export default function Home() {
   return (
     <main className="flex justify-center bg-green-300 w-full text-3xl">
@@ -7,6 +9,8 @@ export default function Home() {
       <h3 className="text-green-900 ">what to do?</h3>
 
       <h1 className="text-red-800"> I'm a PROBLEM LOL! </h1>
+      
+
     </main>
   );
 }
